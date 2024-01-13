2a8ce633a4bd9636537851875129ee556e2cf594
maximilianoarevalo <maximiliano.arevalo@usach.cl>
2024-01-13 16:24:13 -0300

    Se agrega funcion y logica para obtener el total

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-precio = 400000
+precio = 400000;
 
 precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+precioSpan.innerHTML = precio;
 
 let cantidad = document.querySelector(".cantidad").innerHTML;
-let total = document.querySelector(".valor-total");
+let total = document.querySelector(".valor-total").innerHTML;
 
 function aumentarProducto(){
     let cantidadActual = parseInt(cantidad);
     cantidadActual+=1;
     cantidad = cantidadActual;
     document.querySelector(".cantidad").innerHTML = cantidad;
+    actualizarTotal(precio, cantidad);
 }
 
 function disminuirProducto(){
@@ -25,8 +26,15 @@ function disminuirProducto(){
         cantidad = cantidadActual;
         document.querySelector(".cantidad").innerHTML = cantidad;
     }
+    actualizarTotal(precio, cantidad);
     // Si no se quisiera considerar la validacion, aunque eso permita una cantidad de productos no negativa, el codigo seria el siguiente para ambos casos
     // cantidadActual-=1;
     // cantidad = cantidadActual;
     // document.querySelector(".cantidad").innerHTML = cantidad;
+}
+
+function actualizarTotal(precio, cantidad){
+    let resultado = precio * cantidad;
+    total = resultado;
+    document.querySelector(".valor-total").innerHTML = total;
 }
